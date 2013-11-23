ebd01fc4babe4c8b7abb7871960d787dff86eb61
tsle <les.tuan@gmail.com>
2013-11-23 15:47:36 -0500

    fix questions controller

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,18 +1,17 @@
 class Api::QuestionsController <  Api::ProtectedResourceController
 
-		def index
-		end
-
-		def create
-			Question.create!(question_params)
-		end
-
-		private
-
-		def question_params
-			params[:category_id] = Category.find_by!(name: params[:category]) unless params[:category].blank?
-			params.permit(:content,:category_id)
-		end
-
+	def index
 	end
+
+	def create
+		Question.create!(question_params)
+	end
+
+	private
+
+	def question_params
+		params[:category_id] = Category.find_by!(name: params[:category]) unless params[:category].blank?
+		params.permit(:content,:category_id)
+	end
+
 end
