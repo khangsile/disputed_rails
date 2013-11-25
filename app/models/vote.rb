9e290115220c4b546def57f3b908933665c2421c
tsle <les.tuan@gmail.com>
2013-11-24 19:26:55 -0500

    add vote validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
 	belongs_to :question, counter_cache: :votes_count
 	belongs_to :answer, counter_cache: :votes_count
 
-	# validates_uniqueness_of :question_id, scope: :user_id
+	validates_uniqueness_of :question_id, scope: :user_id
 end
