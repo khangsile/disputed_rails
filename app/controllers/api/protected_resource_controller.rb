ddadd4c51f43af848714e85564ef57b473cf8f0e
tsle <les.tuan@gmail.com>
2014-01-05 13:49:48 -0500

    fix token auth

--- app/controllers/api/protected_resource_controller.rb
+++ app/controllers/api/protected_resource_controller.rb
@@ -1,7 +1,7 @@
 module Api
 	class Api::ProtectedResourceController < Api::BaseController
 		include TokenAuthenticatableHandler
-		
-		before_filter authenticate_user_from_token!
+
+		before_filter { authenticate_user_from_token! }
 	end
 end
